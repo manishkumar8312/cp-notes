0dd5ab6590d76bb203a037a5b67010f8d83e2894
Manish Kumar Sah <codeisfunforme@gmail.com>
2026-02-17 19:02:09 +0530

    counting rooms

--- Graphs/CSES Problems/counting rooms.cpp
+++ Graphs/CSES Problems/counting rooms.cpp
@@ -1,3 +1,5 @@
+// DFS Implementation
+
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -49,3 +51,76 @@ int main() {
     cout << rooms << "\n";
     return 0;
 }
+
+
+
+// BFS Implementation
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+
+    vector<string> grid(n);
+    for (int i = 0; i < n; i++) {
+        cin >> grid[i];
+    }
+
+    vector<vector<bool>> visited(n, vector<bool>(m, false));
+
+    // directions: up, down, left, right
+    vector<vector<int>> directions{
+        {-1, 0},
+        {1, 0},
+        {0, -1},
+        {0, 1}
+    };
+
+    int rooms = 0;
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+
+            if (grid[i][j] == '.' && !visited[i][j]) {
+                rooms++;
+
+                queue<pair<int,int>> q;
+                q.push({i, j});
+                visited[i][j] = true;
+
+                while (!q.empty()) {
+                    auto front = q.front();
+                    q.pop();
+                    int r = front.first;
+                    int c = front.second;
+                    for (auto &d : directions) {
+                        int nr = r + d[0];
+                        int nc = c + d[1];
+
+                        if (nr >= 0 && nr < n &&
+                            nc >= 0 && nc < m &&
+                            grid[nr][nc] == '.' &&
+                            !visited[nr][nc]) {
+
+                            visited[nr][nc] = true;
+                            q.push({nr, nc});
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    cout << rooms << endl;
+    return 0;
+}
+
+
+
+
+
+
+
+
