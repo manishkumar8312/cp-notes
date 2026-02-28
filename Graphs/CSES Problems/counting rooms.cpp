de0612ea497c0375136a8fe3408b7bde1f01bcb9
Manish Kumar Sah <codeisfunforme@gmail.com>
2026-02-28 15:05:38 +0530

    counting rooms

--- Graphs/CSES Problems/counting rooms.cpp
+++ Graphs/CSES Problems/counting rooms.cpp
@@ -1,5 +1,29 @@
-// DFS Implementation
+You are given a map of a building, and your task is to count the number of its rooms. The size of the 
+map is n x m squares, and each square is either floor or wall. You can walk left, right, up, and 
+down through the floor squares.
 
+Input
+The first input line has two integers n and m: the height and width of the map.
+Then there are n lines of m characters describing the map. Each character is either . (floor) or # (wall).
+
+Output
+Print one integer: the number of rooms. 
+
+Input
+5 8
+########
+#..#...#
+####.#.#
+#..#...#
+########
+
+Output
+3
+
+Constraints
+1 <= n,m <= 1000
+
+// DFS Implementation
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -56,66 +80,66 @@ int main() {
 
 // BFS Implementation
 
-// #include <bits/stdc++.h>
-// using namespace std;
+#include <bits/stdc++.h>
+using namespace std;
 
-// int main() {
-//     int n, m;
-//     cin >> n >> m;
+int main() {
+    int n, m;
+    cin >> n >> m;
 
-//     vector<string> grid(n);
-//     for (int i = 0; i < n; i++) {
-//         cin >> grid[i];
-//     }
+    vector<string> grid(n);
+    for (int i = 0; i < n; i++) {
+        cin >> grid[i];
+    }
 
-//     vector<vector<bool>> visited(n, vector<bool>(m, false));
+    vector<vector<bool>> visited(n, vector<bool>(m, false));
 
-//     // directions: up, down, left, right
-//     vector<vector<int>> directions{
-//         {-1, 0},
-//         {1, 0},
-//         {0, -1},
-//         {0, 1}
-//     };
+    // directions: up, down, left, right
+    vector<vector<int>> directions{
+        {-1, 0},
+        {1, 0},
+        {0, -1},
+        {0, 1}
+    };
 
-//     int rooms = 0;
+    int rooms = 0;
 
-//     for (int i = 0; i < n; i++) {
-//         for (int j = 0; j < m; j++) {
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
 
-//             if (grid[i][j] == '.' && !visited[i][j]) {
-//                 rooms++;
+            if (grid[i][j] == '.' && !visited[i][j]) {
+                rooms++;
 
-//                 queue<pair<int,int>> q;
-//                 q.push({i, j});
-//                 visited[i][j] = true;
+                queue<pair<int,int>> q;
+                q.push({i, j});
+                visited[i][j] = true;
 
-//                 while (!q.empty()) {
-//                     auto front = q.front();
-//                     q.pop();
-//                     int r = front.first;
-//                     int c = front.second;
-//                     for (auto &d : directions) {
-//                         int nr = r + d[0];
-//                         int nc = c + d[1];
+                while (!q.empty()) {
+                    auto front = q.front();
+                    q.pop();
+                    int r = front.first;
+                    int c = front.second;
+                    for (auto &d : directions) {
+                        int nr = r + d[0];
+                        int nc = c + d[1];
 
-//                         if (nr >= 0 && nr < n &&
-//                             nc >= 0 && nc < m &&
-//                             grid[nr][nc] == '.' &&
-//                             !visited[nr][nc]) {
+                        if (nr >= 0 && nr < n &&
+                            nc >= 0 && nc < m &&
+                            grid[nr][nc] == '.' &&
+                            !visited[nr][nc]) {
 
-//                             visited[nr][nc] = true;
-//                             q.push({nr, nc});
-//                         }
-//                     }
-//                 }
-//             }
-//         }
-//     }
+                            visited[nr][nc] = true;
+                            q.push({nr, nc});
+                        }
+                    }
+                }
+            }
+        }
+    }
 
-//     cout << rooms << endl;
-//     return 0;
-// }
+    cout << rooms << endl;
+    return 0;
+}
 
 
 
